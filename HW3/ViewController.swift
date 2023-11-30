4a124dff414e649508d99f13e004b2cead4683be
Nur <mirrorvol@gmail.com>
2023-11-30 23:00:48 +0400

    Firs version

--- HW3/ViewController.swift
+++ HW3/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var screenRGB: UIView!
+    @IBOutlet var labelBlue: UILabel!
+    @IBOutlet var labelGreen: UILabel!
+    @IBOutlet var labelRed: UILabel!
 
+    
+    @IBOutlet var switchRed: UISlider!
+    @IBOutlet var switchGreen: UISlider!
+    @IBOutlet var switchBlue: UISlider!
+    
+    @IBOutlet var textFieldRed: UITextField!
+    @IBOutlet var textFieldBlue: UITextField!
+    @IBOutlet var textFieldGreen: UITextField!
+    var alphaOfRed: CGFloat = 0.0
+    var alphaOfGreen: CGFloat = 0.0
+    var alphaOfBlue: CGFloat = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func switchRedMuved() {
+        
+        alphaOfRed = CGFloat(switchRed.value)
+        screenRGB.backgroundColor = .red.withAlphaComponent(alphaOfRed)
+        labelRed.text = String(switchRed.value)
+    }
+    @IBAction func switchGreenMuved(_ sender: Any) {
+    }
+    @IBAction func switchBlueMuved(_ sender: Any) {
+    }
+    @IBAction func textFieldRedWrited() {
+    }
+    
+    @IBAction func textFieldGreenWrited() {
+    }
+    
+    @IBAction func textFieldBlueWrited() {
+    }
 }
 
